2fdd02098fcd36d66d6c0e1c26bda2afbf106c1b
Chwizdo <chwizdo@gmail.com>
2022-08-12 11:04:56 +0800

    feat(orders): Admin view and update orders

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/<order_id>/', views.AdminOrderDetailView.as_view(), name='a_order_detail'),
     path('admin/', views.AdminOrderView.as_view(), name='a_order'),
     path('<order_id>/', views.CustomerOrderDetailView.as_view(), name='c_order_detail'),
     path('', views.CustomerOrderView.as_view(), name='c_order'),
--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from orders.models import Address, Order, OrderItem
 
 from carts.models import Cart, CartItem
+from utils.views import Util
 
 
 class CustomerOrderView(View):
@@ -124,3 +125,101 @@ class AdminOrderView(View):
         orders = Order.objects.all()
 
         return render(request, 'a_order_view.html', {'orders': orders})
+
+
+class AdminOrderDetailView(View):
+    def get(self, request, order_id):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
+
+        if unauthenticated_res is not None:
+            return unauthenticated_res
+
+        # If product id is not present in the path parameter,
+        # redirect user back to product listing page.
+        if order_id is None:
+            return redirect('a_order')
+
+        # Query product object from product id in path parameter,
+        # if product object is not found, redirect user back to product listing page.
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return redirect('a_order')
+
+        order_items = OrderItem.objects.filter(order_id=order_id)
+
+        # Loop all cart item and calculate the total payable amount.
+        total_amount = 0
+        for order_item in order_items:
+            total_amount = total_amount + order_item.price * order_item.quantity
+        
+        address = Address.objects.get(order_id=order.id)
+
+        return render(request, 'a_order_detail_view.html', {'order': order, 'order_items': order_items, 'total_amount': total_amount, 'address': address})
+
+    def post(self, request, order_id):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
+
+        if unauthenticated_res is not None:
+            return unauthenticated_res
+
+        # If order id is not present in the path parameter,
+        # redirect user back to order listing page.
+        if order_id is None:
+            return redirect('a_order')
+
+        # Query order object from order id in path parameter,
+        # if order object is not found, redirect user back to order listing page.
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return redirect('a_order')
+
+        order_items = OrderItem.objects.filter(order_id=order_id)
+
+        # Loop all order item and calculate the total payable amount.
+        total_amount = 0
+        for order_item in order_items:
+            total_amount = total_amount + order_item.price * order_item.quantity
+
+        # if more than one address is retrieved, cancel the operation.
+        try:
+            address = Address.objects.get(order_id=order_id)
+        except:
+            return render(request, 'a_order_detail_view.html', {'order': order, 'order_items': order_items, 'total_amount': total_amount, 'address': address})
+
+        method = request.POST.get('_method')
+
+        if method == 'PUT':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+
+            street = request.POST.get('street')
+            city = request.POST.get('city')
+            postcode = request.POST.get('postcode')
+            state = request.POST.get('state')
+            country = request.POST.get('country')
+
+            shipping = request.POST.get('shipping')
+            remark = request.POST.get('remark')
+
+            order.name = name
+            order.email = email
+            order.phone = phone
+            order.shipping = shipping
+            order.remark = remark
+            order.save()
+
+            address.street = street
+            address.city = city
+            address.postcode = postcode
+            address.state = state
+            address.country = country
+            address.save()
+
+        return render(request, 'a_order_detail_view.html', {'order': order, 'order_items': order_items, 'total_amount': total_amount, 'address': address})
+
+            
+
+

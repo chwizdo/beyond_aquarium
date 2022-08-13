8534638891ce435e97393a7d02efcc87840b1742
Chwizdo <chwizdo@gmail.com>
2022-08-13 23:10:33 +0800

    feat(appointments): Customer create new appointment

--- appointments/views.py
+++ appointments/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
+from .models import Appointment
 from utils.views import Util
 from django.views import View
+import datetime
 
 class CustomerContactUsView(View):
-  def get(self, request):
-    unauthenticated_res = Util.redirect_if_unauthenticated(request)
+    def get(self, request):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
 
-    if unauthenticated_res is not None:
-        return unauthenticated_res
+        if unauthenticated_res is not None:
+            return unauthenticated_res
 
-    return render(request, 'c_contact_us_view.html')
+        return render(request, 'c_contact_us_view.html')
+
+
+    def post(self, request):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
+
+        if unauthenticated_res is not None:
+            return unauthenticated_res
+
+        method = request.POST.get('_method')
+
+        if method == 'appointment':
+            date = request.POST.get('date')
+            reason = request.POST.get('reason')
+
+            date_splitted = date.split('-')
+            date = datetime.date(int(date_splitted[0]), int(date_splitted[1]), int(date_splitted[2]))
+
+            Appointment.objects.create(user_id=request.user.id, date=date, reason=reason)
+
+        return render(request, 'c_contact_us_view.html')

d225e2cd77a0c61a742ab8892234d99a7b1416ab
Chwizdo <chwizdo@gmail.com>
2022-08-13 23:28:44 +0800

    feat(appointments): Admin view appointments and feedbacks

--- appointments/views.py
+++ appointments/views.py
@@ -3,6 +3,7 @@ from .models import Appointment, Feedback
 from utils.views import Util
 from django.views import View
 import datetime
+from auths.models import Role
 
 class CustomerContactUsView(View):
     def get(self, request):
@@ -36,3 +37,31 @@ class CustomerContactUsView(View):
             Feedback.objects.create(user_id=request.user.id, content=feedback)
 
         return render(request, 'c_contact_us_view.html')
+
+
+class AdminAppointmentView(View):
+    def get(self, request):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
+
+        if unauthenticated_res is not None:
+            return unauthenticated_res
+
+        appointments = Appointment.objects.all()
+
+        role = Role.objects.get(user_id=request.user.id)
+
+        return render(request, 'a_appointment_view.html', {'role': role.role, 'appointments': appointments})
+
+
+class AdminFeedbackView(View):
+    def get(self, request):
+        unauthenticated_res = Util.redirect_if_unauthenticated(request)
+
+        if unauthenticated_res is not None:
+            return unauthenticated_res
+
+        feedbacks = Feedback.objects.all()
+
+        role = Role.objects.get(user_id=request.user.id)
+
+        return render(request, 'a_feedback_view.html', {'role': role.role, 'feedbacks': feedbacks})
